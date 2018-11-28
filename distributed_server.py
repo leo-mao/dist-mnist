668777110123b97cf1831630c638f1bca672f189
leo-mao <emicool.my@gmail.com>
2018-11-28 14:14:01 +0800

    add more `tf.summary` indications

--- distributed_server.py
+++ distributed_server.py
@@ -5,15 +5,13 @@ import mnist_inference
 import tempfile
 from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 
-# import os
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 flags = tf.app.flags
 
 flags.DEFINE_string('data_dir', None, 'Directory for mnist data')
 flags.DEFINE_integer('hidden_units', 100, '')
-flags.DEFINE_integer('training_steps', 3000, '')
-flags.DEFINE_integer('batch_size', 100, '')
+flags.DEFINE_integer('training_steps', 30000, '')
+flags.DEFINE_integer('batch_size', 50, '')
 flags.DEFINE_float('learning_rate_base', 1e-04, '')
 flags.DEFINE_float('learning_rate_decay', 0.99, '')
 flags.DEFINE_float('moving_average_decay', 0.99, '')
@@ -35,57 +33,65 @@ IMAGE_PIXELS = 28
 
 
 def build_model(x, y_, n_workers, is_chief):
-    regularizer = tf.contrib.layers.l2_regularizer(FLAGS.regularaztion_rate)
-    y = mnist_inference.inference(x, regularizer)
-    tf.summary.histogram('layer2/y', y)
+    with tf.name_scope('regularizer'):
+        regularizer = tf.contrib.layers.l2_regularizer(FLAGS.regularaztion_rate)
+        y = mnist_inference.inference(x, regularizer)
+        tf.summary.histogram('layer2/y', y)
 
     global_step = tf.train.get_or_create_global_step()
-    # Accuracy
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-    with tf.name_scope('loss_function'):
-        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1))
-        cross_entropy_mean = tf.reduce_mean(cross_entropy)
-        loss = cross_entropy_mean + tf.add_n(tf.get_collection('losses'))
-    tf.summary.scalar('layer2/loss', loss)
-
-    with tf.name_scope('train_step'):
+    # init=tf.global_variables_initializer()
+    with tf.name_scope('learning_rate'):
         learning_rate = tf.train.exponential_decay(
             FLAGS.learning_rate_base,
             global_step,
-            60000 / FLAGS.batch_size,  # 60000???TODO mnist.train.num_examples, every batch decays some extent
+            FLAGS.training_steps,  # 60000???TODO mnist.train.num_examples, every batch decays some extent
             FLAGS.learning_rate_decay)
 
+    with tf.name_scope('cross_entropy'):
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1))
+        cross_entropy_mean = tf.reduce_mean(cross_entropy)
+        loss = cross_entropy_mean + tf.add_n(tf.get_collection('losses'))
+        tf.summary.scalar('cross entropy', loss)
+
+    with tf.name_scope('back_propagation'):
         # tf.train.SyncReplicasOptimizer
         opt = tf.train.SyncReplicasOptimizer(
             tf.train.GradientDescentOptimizer(learning_rate),
             replicas_to_aggregate=n_workers,
             total_num_replicas=n_workers
         )
-        sync_replicas_hook = opt.make_session_run_hook(is_chief)
         train_op = opt.minimize(loss, global_step=global_step)
+    with tf.name_scope('accuracy_evaluation'):
+        with tf.name_scope('correct_prediction'):
+            correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+        with tf.name_scope('accuracy'):
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    with tf.name_scope('sync_replicas_hook'):
+        sync_replicas_hook = opt.make_session_run_hook(is_chief)
 
         if is_chief:
             variable_averages = tf.train.ExponentialMovingAverage(FLAGS.moving_average_decay, global_step)
             variables_averages_op = variable_averages.apply(tf.trainable_variables())
             with tf.control_dependencies([variables_averages_op, train_op]):
                 train_op = tf.no_op()
-    tf.summary.scalar('model/accuracy', accuracy)
-    tf.summary.histogram('learning_rate', learning_rate)
+
+        tf.summary.scalar('model/accuracy', accuracy)
+        tf.summary.histogram('learning_rate', learning_rate)
     merge = tf.summary.merge_all()
 
     return merge, global_step, loss, train_op, sync_replicas_hook, accuracy
 
 
-def main(argv=None):
+def main(argv):
+
     ps_hosts = FLAGS.ps_hosts.split(',')
     worker_hosts = FLAGS.worker_hosts.split(',')
     n_workers = len(worker_hosts)
     cluster = tf.train.ClusterSpec({'ps': ps_hosts, 'worker': worker_hosts})
 
     server = tf.train.Server(cluster, job_name=FLAGS.job_name, task_index=FLAGS.task_index)
+
     if FLAGS.job_name == 'ps':
         with tf.device("/cpu:0"):
             server.join()
@@ -96,14 +102,15 @@ def main(argv=None):
         worker_device='/job:worker/task:%d' % FLAGS.task_index, cluster=cluster)
 
     with tf.device(device_setter):
-        # Design Graph
         with tf.name_scope('input'):
             x = tf.placeholder(tf.float32, [None, mnist_inference.INPUT_NODE], name='x-input')
             y_ = tf.placeholder(tf.float32, [None, mnist_inference.OUTPUT_NODE], name='y-input')
         summary, global_step, loss, train_op, sync_replicas_hook, accuracy = build_model(x, y_, n_workers, is_chief)
 
-        hooks = [sync_replicas_hook, tf.train.StopAtStepHook(last_step=FLAGS.training_steps)]
-        sess_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
+        with tf.name_scope('stop_hook'):
+            hooks = [sync_replicas_hook, tf.train.StopAtStepHook(last_step=FLAGS.training_steps)]
+        with tf.name_scope('Session_config'):
+            sess_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
 
     # input output
     with tf.train.MonitoredTrainingSession(master=server.target,
@@ -130,9 +137,6 @@ def main(argv=None):
                              "loss on training batch is %g. (%.3f sec/batch)" + \
                              "\n Accuracy on test set is (%.4f )"
                 print(format_str % (step, global_step_value, loss_value, sec_per_batch, accuracy_value))
-
-                # if step < FLAGS.training_steps:
-                #     print('Accuracy: {}'.format(accuracy_value))
 
             step += 1
     train_write.close()
--- mnist_inference.py
+++ mnist_inference.py
@@ -5,22 +5,43 @@ OUTPUT_NODE = 10
 LAYER1_NODE = 500
 
 
-def get_weight_variable(shape, regularizer):
-    weights = tf.get_variable("weights", shape, initializer=tf.truncated_normal_initializer(stddev=0.1))
-    if regularizer is not None:
-        tf.add_to_collection('losses', regularizer(weights))
+def variable_summaries(var, name):
+    with tf.name_scope('summaries'):
+        tf.summary.histogram(name, var)
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean/' + name, mean)
+        stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev/' + name, stddev)
+
+
+def create_nn_layer(input_tensor, input_dim, output_dim, layer_name, regularizer, act=tf.nn.relu):
+    with tf.name_scope(layer_name):
+        with tf.name_scope(layer_name):
+            weights = get_weight_variable(regularizer, input_dim, output_dim)
+            variable_summaries(weights, layer_name + '/weights')
+        with tf.name_scope('biases'):
+            biases = tf.Variable(tf.constant(0.0, shape=[output_dim]))
+            variable_summaries(biases, layer_name + '/biases')
+        with tf.name_scope('Wx_plus_b'):
+            preactivate = tf.matmul(input_tensor, weights) + biases
+            tf.summary.histogram(layer_name + '/pre_activations', preactivate)
+
+        activations = act(preactivate, name='activation')
+        tf.summary.histogram(layer_name + '/activation', activations)
+        return activations
+
+
+def get_weight_variable(regularizer, input_dim, output_dim):
+    with tf.name_scope('initialize_weights'):
+        weights = tf.Variable(tf.truncated_normal([input_dim, output_dim], stddev=0.1))
+        if regularizer is not None:
+            tf.add_to_collection('losses', regularizer(weights))
     return weights
 
 
 def inference(input_tensor, regularizer):
-    with tf.variable_scope('ReLu-1'):
-        weights = get_weight_variable([INPUT_NODE, LAYER1_NODE], regularizer)
-        biases = tf.get_variable("biases", [LAYER1_NODE], initializer=tf.constant_initializer(0.0))
-        layer1 = tf.nn.relu(tf.matmul(input_tensor, weights) + biases)
+    hidden1 = create_nn_layer(input_tensor, INPUT_NODE, LAYER1_NODE, 'fc-1', act=tf.nn.relu, regularizer=regularizer)
 
-    with tf.variable_scope('ReLu-2'):
-        weights = get_weight_variable([LAYER1_NODE, OUTPUT_NODE], regularizer)
-        biases = tf.get_variable("biases", [OUTPUT_NODE], initializer=tf.constant_initializer(0.0))
-        layer2 = tf.matmul(layer1, weights) + biases
+    hidden2 = create_nn_layer(hidden1, LAYER1_NODE, OUTPUT_NODE, 'fc-2', act=tf.nn.relu, regularizer=regularizer)
 
-    return layer2
+    return hidden2
